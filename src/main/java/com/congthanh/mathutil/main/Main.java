18deb7aff53c3e87ba7e45ed2ae5746d7ba758a8
ThanhAmery <dct01112004@gmail.com>
2024-08-29 19:15:01 +0700

    1st upload with maven project

---
+++ src/main/java/com/congthanh/mathutil/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.congthanh.mathutil.main;
+
+import com.congthanh.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+        
+    }
+}
